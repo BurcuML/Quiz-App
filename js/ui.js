291fdbfb8f25e8a34d55280e1dc465ba38aa74ed
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2026-01-11 22:54:06 +0300

    Added CSS styles for quiz box header and updated JavaScript code for quiz functionality

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,16 @@
 function UI(){
-    this.quiz_box = document.querySelector("#quiz-box")
+    this.quizBox = document.querySelector("#quiz-box")
+    this.btnBox = document.querySelector("#button-box")
+    this.scoreBox = document.querySelector("#score-box")
     this.body = document.querySelector("#quiz-box #body")
     this.correctIcon = '<i class="bi bi-check-circle"></i>'
     this.incorrectIcon = '<i class="bi bi-x-circle"></i>'
+    this.btnStart = document.querySelector(".btn-start")
     this.btnNext = document.querySelector(".btn-next")
     this.btnReplay = document.querySelector(".btn-replay")
     this.btnQuit = document.querySelector(".btn-quit")
+    this.timeText = document.querySelector(".time-text")
+    this.timeSecond = document.querySelector(".time-second")
 }
 
 UI.prototype.showQuestion = function(question){
